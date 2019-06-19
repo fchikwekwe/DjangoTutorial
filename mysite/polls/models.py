b04c81008578a4dc3bd534c4c4cf6dd5659ed6e0
Faith Chikwekwe <faithchikwekwe01@gmail.com>
2019-06-18 19:28:46 -0700

    ✨ adding more views to polls app

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -5,8 +5,13 @@ class Question(models.Model):
         # Questions is the optional human redable name for this ForeignKey
     pub_date =  models.DateTimeField('date published')
 
-class Choice(models.Model):
+    def __str__(self):
+        return self.question_text
 
+class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choice_text
